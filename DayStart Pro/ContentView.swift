ca907fbec9acfba6fd6d79f366eab3a21c7c7407
Charlie Larson <clarson@lcs.on.ca>
2024-05-27 15:05:58 -0400

    Initial Commit

---
+++ DayStart Pro/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DayStart Pro
+//
+//  Created by Charlie Larson on 2024-05-27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DayStart Pro/DayStart_ProApp.swift
@@ -0,0 +1,17 @@
+//
+//  DayStart_ProApp.swift
+//  DayStart Pro
+//
+//  Created by Charlie Larson on 2024-05-27.
+//
+
+import SwiftUI
+
+@main
+struct DayStart_ProApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
