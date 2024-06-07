e528fa241cdbd1dfd306a66c55c751c07c15413b
Charlie Larson <clarson@lcs.on.ca>
2024-06-07 12:02:50 -0400

    Added a helper file to identify our app to the Supabase servers.

---
+++ DayStart Pro/Helpers/Supabase.swift
@@ -0,0 +1,15 @@
+//
+//  Supabase.swift
+//  DayStart Pro
+//
+//  Created by Charlie Larson on 2024-06-07.
+//
+
+import Foundation
+import Supabase
+
+let supabase = SupabaseClient(
+  supabaseURL: URL(string: "https://mridogtrjowhdwksnnqs.supabase.co")!,
+  supabaseKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1yaWRvZ3Ryam93aGR3a3NubnFzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTc2ODExNjYsImV4cCI6MjAzMzI1NzE2Nn0.ciOxs2Gr_7hmfaaP63VwMaKhdprVH4HJa1hE-M16t8w"
+)
+
